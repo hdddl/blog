ce37edf53e4efedfe71bd57e9503fd45360ac0cd
HuangDongliu-DL <dl.wangdongliu@gmail.com>
2022-02-05 20:30:35 +0800

    完成生产环境配置

--- blog/settings.py
+++ blog/settings.py
@@ -111,8 +111,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+from os import path
 
 STATIC_URL = '/static/'
+STATIC_ROOT = path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
