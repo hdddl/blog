bcdb6250bf2b1d63c581494a3015ba82baf84fff
dl.wangdongliu@gmail.com <g280417484g>
2022-03-13 08:43:51 +0800

    admin页面增加图片预览功能

--- blog/settings.py
+++ blog/settings.py
@@ -95,7 +95,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'zh-hans'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'Asia/Shanghai'
 
--- img/admin.py
+++ img/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-
+from django.utils.safestring import mark_safe
 from .models import Image, Album
+from blog.env import HOST
 
 
 # Register your models here.
@@ -8,6 +9,15 @@ from .models import Image, Album
 
 class ImageAdmin(admin.ModelAdmin):
     list_display = ("name", "album", "pubdate")
+    readonly_fields = ("image_preview",)
+
+    @admin.display(description="preview")
+    def image_preview(self, instance):
+        """
+        展示图片预览
+        """
+        url = HOST + "/media" +  instance.image.url
+        return mark_safe('<img src="%s">'%url)
 
 
 admin.site.register(Image, ImageAdmin)
