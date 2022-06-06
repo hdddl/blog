36c89bd34d76fe0478a7678fdde743707ee008c8
huangdongliu <dongliu@dongliu.site>
2022-06-06 17:11:26 +0800

    支持为图片上传插件返回直链

--- img/views.py
+++ img/views.py
@@ -20,21 +20,23 @@ def upload(request):
         cnt = 1
         for i in album_object:
             context["album"][str(cnt)] = i['name']
-            cnt = cnt+1
+            cnt = cnt + 1
         return render(request, 'upload.html', context=context)
     else:
         context = {
-            "direct_Link" :"",
+            "direct_Link": "",
             "Markdown_Link": "",
             "HTML_Link": "",
         }
         form = UploadImage(request.POST, request.FILES)
         if form.is_valid():
             entry = form.save()
-            imageUrl = HOST + "/media" + entry.image.url     # 获取直链
-            imageName = entry.image.name.split("/")[-1]     # 获取图片名称
+            imageUrl = HOST + "/media" + entry.image.url  # 获取直链
+            imageName = entry.image.name.split("/")[-1]  # 获取图片名称
             imageName = imageName.split(".")[0]
             context["direct_Link"] = imageUrl
             context["Markdown_Link"] = "![%s](%s)" % (imageName, imageUrl)
             context["HTML_Link"] = "<img src=\"%s\" alt=\"%s\">" % (imageUrl, imageName)
+            if "direct" in request.headers:
+                return JsonResponse({"status": True, "path": entry.image.url})
         return render(request=request, template_name="uploadSuccess.html", context=context)
