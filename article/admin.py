343db122d743f3749ac35ac7ed433d4bcf35fbf8
huangdongliu <dongliu@dongliu.site>
2022-07-07 16:32:37 +0800

    支持批量更新博客与微博

--- article/admin.py
+++ article/admin.py
@@ -14,35 +14,50 @@ class auto_update(admin.ModelAdmin):
         if obj.__class__ == Pages and obj.page_type.name == "html":
             super().save_model(request, obj, form, change)
             return
-        if obj.__class__ == Micro_blog:     # 如果是微博
+        if obj.__class__ == Micro_blog:  # 如果是微博
             html = markdown2html(obj.markdown_text, template=False, standalone=False)
-        else:       # 如果是博客
+        else:  # 如果是博客
             html = markdown2html(obj.markdown_text, template=True, standalone=True)
         obj.html_text = html
         obj.save()
         super().save_model(request, obj, form, change)
 
 
+# 批量更新博客
+def update_blog(modeladmin, request, queryset):
+    for obj in queryset:
+        obj.html_text = markdown2html(obj.markdown_text, True, True)
+
+
 class BlogAdmin(auto_update):
     list_display = ('title', 'category', 'visits', 'pubdate', 'public')
     exclude = ('visits', 'html_text', 'updated')
+    actions = [update_blog]
+
+
+# 生成页面跳转URL
+def page_url(obj):
+    url = HOST + "/api/pages?name=" + obj.title
+    return mark_safe('<a href="%s" target=”_blank”>redirect</a>' % url)
 
 
 class PagesAdmin(auto_update):
-    # 返回页面所对应的URL
-    def page_url(self, obj):
-        url = HOST + "/api/pages?name=" + obj.title
-        return mark_safe('<a href="%s" target=”_blank”>redirect</a>' % url)
-
-    list_display = ('title', 'page_type', 'pubdate', "page_url")
+    list_display = ('title', 'page_type', 'pubdate', page_url)
     list_filter = ('title', 'page_type', 'pubdate')
     exclude = ('updated',)
+
+
+# 批量更新博客
+def update_micro_blog(modeladmin, request, queryset):
+    for obj in queryset:
+        obj.html_text = markdown2html(obj.markdown_text, False, False)
 
 
 class MicroBlogAdmin(auto_update):
     list_display = ('description', 'pubdate', 'public')
     list_filter = ('pubdate',)
     exclude = ('html_text', 'updated')
+    actions = [update_micro_blog]
 
 
 admin.site.register(Blog, BlogAdmin)
